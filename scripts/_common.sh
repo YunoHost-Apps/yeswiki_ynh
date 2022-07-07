569efe3ed3886ea5c3318a925c78756abbacff26
yalh76 <yalh@yahoo.com>
2022-07-07 03:08:58 +0200

    Upgrade to 4.2.1~ynh1

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@
 YNH_PHP_VERSION="7.3"
 
 # dependencies used by the app
-extra_php_dependencies="php${YNH_PHP_VERSION}-zip"
+pkg_dependencies="php${YNH_PHP_VERSION}-zip"
 
 loginldap_version="2021-03-01-2"
 
