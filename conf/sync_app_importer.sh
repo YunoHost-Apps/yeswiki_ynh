86c820e67097440c351f69531a9ed04bc4afc1c7
mrflos <mrflos@gmail.com>
2024-09-04 10:02:32 +0300

    fix(sync): adapt importers to new helpers

--- conf/sync_app_importer.sh
+++ conf/sync_app_importer.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-sudo -u __USER__ bash -c "cd __INSTALL_DIR__ && ./yeswicli importer:sync -s yunohost-apps"
+sudo -u __APP__ bash -c "cd __INSTALL_DIR__ && ./yeswicli importer:sync -s yunohost-apps"
+
--- conf/sync_user_importer.sh
+++ conf/sync_user_importer.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo -u __USER__ bash -c "cd __INSTALL_DIR__ && ./yeswicli importer:sync -s yunohost-users"
+sudo -u __APP__ bash -c "cd __INSTALL_DIR__ && ./yeswicli importer:sync -s yunohost-users"
