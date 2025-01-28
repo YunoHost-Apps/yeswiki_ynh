cc5d588f32f5ecb6df37997a4175c18e63b8c07e
Florian Schmitt <mrflos@gmail.com>
2025-01-28 11:58:19 +0300

    feat(install/upgrade): add a cache file with yunohost version

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,3 +5,8 @@
 #=================================================
 
 composer_version=2.8.1
+
+cache_yunohost_version() {
+  (cd "$install_dir" && ynh_exec_as_app \
+      dpkg-query --show --showformat='${Version}' yunohost > files/yunohost_version)
+}
